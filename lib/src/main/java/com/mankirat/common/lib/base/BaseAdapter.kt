f1d5e5aa8dcea3e0ed03c046dcbadad21f0b297f
mankirat <mankirat.dev@gmail.com>
2022-06-08 13:01:46 +0530

    BaseAdapter remove notifyChanged

--- lib/src/main/java/com/mankirat/common/lib/base/BaseAdapter.kt
+++ lib/src/main/java/com/mankirat/common/lib/base/BaseAdapter.kt
@@ -2,65 +2,93 @@
 
 package com.mankirat.common.lib.base
 
-import android.annotation.SuppressLint
 import android.content.Context
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 
-abstract class BaseAdapter<T, viewBinding : ViewBinding>(val singleSelection: Boolean = false) : RecyclerView.Adapter<BaseAdapter<T, viewBinding>.BaseViewHolder>() {
+abstract class BaseAdapter<model : Any, viewBinding : ViewBinding>(private val singleSelection: Boolean = false) :
+    ListAdapter<model, BaseAdapter<model, viewBinding>.BaseViewHolder>(BaseDiffCallback<model>()) {
 
-    var list: ArrayList<T> = ArrayList()
+    abstract fun setBindViewHolder(binding: viewBinding, model: model, position: Int, context: Context)
 
-    @SuppressLint("NotifyDataSetChanged")
-    open fun setData(list: ArrayList<T>) {
-        this.list = list
-        notifyDataSetChanged()
-    }
+    open fun setViewHolder(binding: viewBinding, viewHolder: BaseViewHolder) {}
 
+    abstract fun areContentsTheSame(oldItem: model, newItem: model): Boolean
+
+    //val diffCallback = BaseDiffCallback(field, value)
+
+//    var data = listOf<model>()
+//        set(value) {
+//            val diffResult = DiffUtil.calculateDiff(diffCallback)
+//            field = value
+//            //notifyDataSetChanged()
+//            diffResult.dispatchUpdatesTo(this)
+//        }
 
     override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
-        val model = list[position]
+        val model = getItem(position)
+        //val model = data[position]
         val binding = holder.binding
+
+        binding.root.setOnClickListener {
+            clickListener?.invoke(model, position)
+            if (singleSelection) selectedPos = position
+        }
 
         setBindViewHolder(binding, model, position, binding.root.context)
     }
-
-    override fun getItemCount() = list.size
-
-
-    abstract fun setBindViewHolder(binding: viewBinding, model: T, position: Int, context: Context)
-
-    open fun setViewHolder(binding: viewBinding, viewHolder: BaseViewHolder) {}
 
 
     inner class BaseViewHolder(val binding: viewBinding) : RecyclerView.ViewHolder(binding.root) {
 
         init {
             setViewHolder(binding, this)
-
-            binding.root.setOnClickListener {
-                val position = adapterPosition
-                if (position == RecyclerView.NO_POSITION) return@setOnClickListener
-
-                clickListener?.invoke(binding, list[position], position)
-
-                if (singleSelection) {
-                    selectedPos = position
-                }
-            }
-
         }
 
     }
 
-    var clickListener: ((view: viewBinding, model: T, position: Int) -> Unit)? = null
+    var clickListener: ((model: model, position: Int) -> Unit)? = null
 
     var selectedPos: Int = 0
         set(value) {
             val oldPos = field
             field = value
             notifyItemChanged(oldPos)
-            notifyItemChanged(field)
+            notifyItemChanged(value)
         }
+
+
+    /*abstract class BaseDiffCallback<model>(private val oldList: List<model>, private val newList: List<model>) : DiffUtil.Callback() {
+
+        override fun getOldListSize() = oldList.size
+
+        override fun getNewListSize() = newList.size
+
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldList[oldItemPosition].aa == newList[newItemPosition].aa
+        }
+
+        override fun areContentsTheSame(oldPosition: Int, newPosition: Int): Boolean {
+            return oldList[oldPosition] == newList[newPosition]
+        }
+
+        @Nullable
+        override fun getChangePayload(oldPosition: Int, newPosition: Int): Any? {
+            return super.getChangePayload(oldPosition, newPosition)
+        }
+    }*/
+
+    class BaseDiffCallback<model : Any> : DiffUtil.ItemCallback<model>() {
+
+        override fun areItemsTheSame(oldItem: model, newItem: model): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: model, newItem: model): Boolean {
+            return areContentsTheSame(oldItem, newItem)
+        }
+    }
 
 }
